7d5c81b3c0c6a986fc9c09dd89167754b1b86617
Ahmet Kadayıfçı <ahmetkadayfc@hotmail.com>
2024-07-07 12:07:09 +0300

    add metadata to dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,16 @@
+import { Metadata } from "next"
 import Link from "next/link"
+
+export const metadata: Metadata = {
+    title: 'Dashboard | Index',
+}
 
 export default function Page() {
     return (
-        <main>
+        <div>
+            <h1>Dashboard Index</h1>
             <Link href="/">Go to Home</Link><br/>
             <Link href="/website">Go to Website</Link>
-        </main>
+        </div>
     )
 }
