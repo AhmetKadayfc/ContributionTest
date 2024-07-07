1be3825553e972ef705e282f5bb00339c785c17c
Ahmet Kadayıfçı <ahmetkadayfc@hotmail.com>
2024-07-07 12:09:33 +0300

    add def classes to dashboard page's container

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 export default function Page() {
     return (
-        <main>
-            <Link href="/">Go to Home</Link><br/>
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
             <Link href="/website">Go to Website</Link>
-        </main>
+            <Link href="/">Go to Home</Link><br/>
+        </div>
     )
 }
